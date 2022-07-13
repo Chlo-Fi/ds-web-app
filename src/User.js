5202191daac1336753c56b14ee2ad91552855233
Chloe Skorpikova <cskorpi@wgu.edu>
2022-07-13 14:39:18 -0700

    Added buttons with console message and counter to User.js

--- src/User.js
+++ src/User.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+
 function User(props) {
     console.log(props);
     return (
-        <h1>Name: {props.name} - Age: {props.age}</h1>
+        <h1>Name: {props.name} - Age: {props.age} <button onClick={props.function}>Click Me</button></h1>
     );
 }
 
 function SecondUser() {
+    const[counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(counter + 1);
+    }
+
     return (
-        <h1>Second User Data Here</h1>
+        <div>
+            <h1>Second User Data Here</h1>
+            <button onClick={increment}>Click Me</button>
+            <p>You Clicked the Button {counter} Times.</p>
+        </div>
+
     )
 }
 
